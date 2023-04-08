d9541ca5cc48766ca8b877a14cca3a1cc19fdc91
Your Name <you@example.com>
2023-04-08 12:36:40 +0100

    APril 8 Commit

--- analysis.py
+++ analysis.py
@@ -1,20 +1,28 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as mpl
+import csv
 
+data = []
+
+with open("Iris-dataset.csv") as f:
+    f.read
+filename = "Irises Summary.txt"
 
 #Drawn from https://stackoverflow.com/questions/33889310/r-summary-equivalent-in-numpy
 
 data = pd.read_csv("Iris-dataset.csv", sep= ",")
 data.head()
 
+species = [[i][0][0] for i in data]
+
+print(data)
+
 summary = data.describe()
 
 
 
-with open("Iris-dataset.csv") as f:
-    f.read
-filename = "Irises Summary.txt"
+
 with open(filename, "wt") as f:
     f.write("Iris Flowers Summary")
     f.write(str(print(summary.head())))
@@ -27,4 +35,4 @@ y = "sepal_length"
 print(x)
 
 scatter = mpl.scatter(x,y)
-mpl.show()
+# mpl.show()
