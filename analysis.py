62ff771fce203dfc68e5f4f7811b06d00d821e67
Your Name <you@example.com>
2023-04-10 12:51:40 +0100

    2nd Commit 10th April - Code Working

--- analysis.py
+++ analysis.py
@@ -3,32 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as mpl
 import csv
 
-# data = []
-
-with open("Iris-dataset.csv") as f:
-   f.read
-filename = "Irises Summary.txt"
-
 #Drawn from https://www.geeksforgeeks.org/python-read-csv-columns-into-list/
 
 data = pd.read_csv("Iris-dataset.csv")
 
+
 sepal_length = data['sepal_length'].tolist()
 sepal_width = data['sepal_width'].tolist()
 petal_length = data['petal_length'].tolist()
-petal_length = data["petal_length"].tolist()
-
-summary = data.describe()
-
-
-with open(filename, "wt") as f:
-    f.write("Iris Flowers Summary")
-    f.write(str(print(summary.head())))
-
+petal_width = data["petal_width"].tolist()
 
 
 x = np.array(sepal_width)
 y = np.array(sepal_length)
 
 scatter = mpl.scatter(x,y)
-mpl.show()
+# mpl.show()
+
+with open("Iris Summary Information.txt", "w") as f:
+    f.write(str(print(sepal_length)))
