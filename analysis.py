afe0aed4d4f522322951c2130ad6efbbe6d41879
Your Name <you@example.com>
2023-04-14 12:12:46 +0100

    Commit of Project Files - All Working OK, Outputs are more asthetic

--- analysis.py
+++ analysis.py
@@ -1,49 +1,63 @@
+# This file analyses the Iris-dataset.csv file, produces some summary statistics and outputs then to a text file, 
+#creates some scatter plots of the variables, and creates and saves some hisograms of each variable.
+#Further info is available in the README file on GitHub.
+#Author: OOS
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as mpl
 import csv
 
 
-
+#This block of code imports the CSV file and converts it to a pandas dataframe.
 # Drawn from https://www.w3schools.com/python/pandas/pandas_csv.asp
 
 data = pd.read_csv("Iris-dataset.csv")
-dataralue = data.round(decimals=2)
+
+summary = data.describe()
+rounded_summary = np.round(summary,2)
+
+with open("Iris Summary Information.txt", "w+") as f:
+    f.write(str(rounded_summary))
 
 # Code taken from https://www.geeksforgeeks.org/python-read-csv-columns-into-list/ and https://stackoverflow.com/questions/9012487/matplotlib-pyplot-savefig-outputs-blank-image
+#This code creates lists out of the columns in the CSV datafile. 
 
 sepal_length = data['sepal_length'].tolist()
 sepal_width = data['sepal_width'].tolist()
 petal_length = data['petal_length'].tolist()
 petal_width = data["petal_width"].tolist()
 
+#The following code creates and displays some (pretty) histograms 
+
 sepal_len_hist = mpl.hist(sepal_length, color="red")
 mpl.title("Sepal Length")
 mpl.xlabel("Length (mm)")
 mpl.ylabel("Occurences")
 mpl.savefig("sepal_len_hist.png", dpi=100)
-#mpl.show()
+mpl.show()
 
 sepal_width_hist = mpl.hist(sepal_width, color="blue")
 mpl.title("Sepal Width")
 mpl.xlabel("Width (mm)")
 mpl.ylabel("Occurences")
 mpl.savefig("sepal_width_hist.png", dpi=100)
-#mpl.show()
+mpl.show()
 
 petal_len_hist = mpl.hist(petal_length, color="orange" )
 mpl.title("Petal Length")
 mpl.xlabel("Length (mm)")
 mpl.ylabel("Occurences")
 mpl.savefig("petal_len_hist.png", dpi=100)
-#mpl.show()
+mpl.show()
 
 petal_width_hist = mpl.hist(petal_width, color="green")
 mpl.title("Petal Width")
 mpl.xlabel("Width (mm)")
 mpl.ylabel("Occurences")
 mpl.savefig("petal_width_hist.png", dpi=100)
-#mpl.show()
+mpl.show()
+
+#The following code creates variables aligned to each of the four variable sin the CSV file, and plots them against one another in scatter plots.
 
 x = np.array(sepal_width)
 y = np.array(sepal_length)
@@ -54,46 +68,34 @@ scatter1 = mpl.scatter(x,y, color="red")
 mpl.title("Sepal Width - Sepal Length")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
+mpl.show()
 
 scatter2 = mpl.scatter(x,z, color="blue")
 mpl.title("Sepal Width - Petal Length")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
+mpl.show()
 
 scatter3 = mpl.scatter(x,a, color="orange")
 mpl.title("Sepal Width - Petal Width")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
+mpl.show()
 
 scatter4 = mpl.scatter(y,z, color="green")
 mpl.title("Sepal Length - Petal Length")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
+mpl.show()
 
 scatter5 = mpl.scatter(y,a, color="purple")
 mpl.title("Sepal Length - Petal Width")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
+mpl.show()
 
 scatter6 = mpl.scatter(z,a, color="yellow")
 mpl.title("Petal Length - Petal Width")
 mpl.xlabel("Length/Width (mm)")
 mpl.ylabel("Length/Width (mm)")
-#mpl.show()
-
-
-sepal_length_mean = np.mean(sepal_length)
-sepal_width_mean = np.mean(sepal_width)
-petal_length_mean = np.mean(petal_length)
-petal_width_mean = np.mean (petal_width)
-
-means = sepal_length_mean, sepal_width_mean, petal_length_mean, petal_width_mean
-summary = data.describe()
-
-with open("Iris Summary Information.txt", "w+") as f:
-    f.write(str(summary))
+mpl.show()
